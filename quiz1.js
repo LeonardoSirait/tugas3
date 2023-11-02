b9843b2d8e75e072cb1bbce93866aa36664532b7
LeonardoSirait <149705810+LeonardoSirait@users.noreply.github.com>
2023-11-03 04:06:15 +0700

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,14 @@
+const restaurant = {
+  name: "Rumah Makan vona",
+  city: "Pematangsiantar",
+  favoriteDrink: "Nutello Chocolate",
+  favoriteFood: "Chicken Stick",
+  isVegan: true,
+};
+
+const name = restaurant.name;
+const favoriteDrink = restaurant.favoriteDrink;
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
+console.log("Makanan Favorit:", restaurant.favoriteFood);
